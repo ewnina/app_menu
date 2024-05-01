d56095dfd8e70ec1706f69e9e18909bac04c84ae
ewnina <edernina@gmail.com>
2024-05-01 17:38:28 -0400

    Update ShoppingCart.tsx

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -68,6 +68,8 @@ const  ShoppingCartModal: React.FC<ShopingCartProps> = ({ toggleModal, isOpen })
             </div>
             <div className="mt-6">
                 <button className="w-full bg-blue-500 text-white p-3 rounded" onClick={toggleModal} >Ordenar</button>
+
+               	<a href="https://wa.me/18293839651?text=Me%20gustaría%20saber%20el%20precio%20del%20coche" class="whatsapp" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i></a>
                 
             </div>
             </div>
